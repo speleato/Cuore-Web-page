dab7967ad6a81dc49e4f710ce4d6659eeb70a33f
klinvill <kjlinvill@gmail.com>
2013-12-16 10:57:42 -0800

    added in code to send verification email to leo

--- pycharm_pyramid/Views/Registration_View.py
+++ pycharm_pyramid/Views/Registration_View.py
@@ -3,8 +3,20 @@ from mako.runtime import Context
 from StringIO import StringIO
 from mako.lookup import TemplateLookup
 from pyramid.response import Response
+from pyramid_mailer import get_mailer
+from pyramid_mailer.message import Message
 
 def pushTemplate(templatename):
     mylookup = TemplateLookup(directories=['Views/templates'])
     mytemplate = mylookup.get_template(templatename)
     return Response(mytemplate.render())
+
+
+#after registration, send email to leo
+mailer=get_mailer(request)
+message = Message(subject="Registration by "+name,
+                  sender=cuore_email,
+                  recipients=["leo@cuore.io"],
+                  body=name+" has registered for the Cuore Intranet with the email "+email+". Click here to confirm "
+                       +name+"'s registration: "+admin_panel)
+mailer.send(message)
