a5839bad2655df745d416900d6694c29b15873ef
Sandy <slhsith@gmail.com>
2013-12-16 21:30:49 -0800

    modified:   cuorewebpage/Views/Registration_View.py 	modified:   cuorewebpage/Views/Registration_View.pyc 	modified:   cuorewebpage/__init__.py 	modified:   cuorewebpage/__init__.pyc 	new file:   cuorewebpage/templates/Login.mako 	deleted:    cuorewebpage/Views/templates/Login.txt 	deleted:    cuorewebpage/Views/templates/Registration.txt 	deleted:    cuorewebpage/main.pyc

--- cuorewebpage/Views/Registration_View.py
+++ cuorewebpage/Views/Registration_View.py
@@ -1,12 +1,47 @@
-from mako.template import Template
-from mako.runtime import Context
-from StringIO import StringIO
-from mako.lookup import TemplateLookup
 from pyramid.response import Response
+from pyramid.view import view_config
 
-def pushTemplate(templatename):
-    mylookup = TemplateLookup(directories=['Views/templates'])
-    mytemplate = mylookup.get_template(templatename)
-    return Response(mytemplate.render())
+@view_config(route_name='Registration', renderer='cuorewebpage:templates/Registration.mako')
+def Registration(request):
+    #parameters = Model.process_business_logic()
+    print "bingo"
+    if request.POST:
+        print "submitted"
+        if request.POST.getone('task') == "admin":
+            title=request.POST.getone('title')
+            email=request.POST.getone('email')
+            department=request.POST.getone('department')
+
+            print "admin path"
+            # add updated info to database
+            # after confirmed by Leo, send email to user
+            mailer = get_mailer(request)
+            message = Message(subject="Confirm your Cuore Intranet Registration",
+                  sender = "kirby@cuore.io",                    # change to admin email later
+                  recipients = [email],
+                  body = "You have been added to the Cuore intranet as a " + title + " in the "
+                       + department + " department. Click on this link to confirm your"
+                       + "registration: " + "user_link")
+            mailer.send(message)
+
+        elif request.POST.getone('task') == "create":
+            name=request.POST.getone('firstName') + " " + request.POST.getone('lastName')
+            email=request.POST.getone('email')
+
+            print "created"
+            # create user node in database, put in temporary zone
+            # after registration, send email to Leo
+            mailer=get_mailer(request)
+            message = Message(subject="Registration by " + name,
+                              sender="kirby@cuore.io",      # change to cuore mail server later
+                              recipients=["kirby@cuore.io"],  # change to leo when rolled out
+                              body=name + " has registered for the Cuore Intranet with the email "
+                                     + email + ". Click here to confirm " + name
+                                     + "'s registration: " + "link_to_admin_panel")
+            mailer.send(message)
+        elif request.POST.getone('task')=="edit":
+            # update info in database
+            print "updated"
+    return {}
 
 
--- cuorewebpage/__init__.py
+++ cuorewebpage/__init__.py
@@ -1,55 +1,19 @@
 from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
-from Model import Model
-from Views import Login_View, Registration_View
+#from Model import Model
+#from Views import Login_View, Registration_View
 from pyramid_mailer import get_mailer
 from pyramid_mailer.message import Message
+from pyramid.response import Response
+from pyramid.view import view_config
 
+@view_config(route_name='Login', renderer='cuorewebpage:templates/Login.mako')
 def Login(request):
     parameters = Model.get_initial_data()
-    return Login_View.pushTemplate('Login.txt', parameters)
+    #return Login_View.pushTemplate('Login.txt', parameters)
+    return {'name': 'John', 'surname': 'Doe'}
 
-
-def Registration(request):
-    parameters = Model.process_business_logic()
-    print "bingo"
-    if request.POST:
-        print "submitted"
-        if request.POST.getone('task') == "admin":
-            title=request.POST.getone('title')
-            email=request.POST.getone('email')
-            department=request.POST.getone('department')
-
-            print "admin path"
-            # add updated info to database
-            # after confirmed by Leo, send email to user
-            mailer = get_mailer(request)
-            message = Message(subject="Confirm your Cuore Intranet Registration",
-                  sender = "kirby@cuore.io",                    # change to admin email later
-                  recipients = [email],
-                  body = "You have been added to the Cuore intranet as a " + title + " in the "
-                       + department + " department. Click on this link to confirm your"
-                       + "registration: " + "user_link")
-            mailer.send(message)
-        elif request.POST.getone('task') == "create":
-            name=request.POST.getone('firstName') + " " + request.POST.getone('lastName')
-            email=request.POST.getone('email')
-
-            print "created"
-            # create user node in database, put in temporary zone
-            # after registration, send email to Leo
-            mailer=get_mailer(request)
-            message = Message(subject="Registration by " + name,
-                              sender="kirby@cuore.io",      # change to cuore mail server later
-                              recipients=["kirby@cuore.io"],  # change to leo when rolled out
-                              body=name + " has registered for the Cuore Intranet with the email "
-                                     + email + ". Click here to confirm " + name
-                                     + "'s registration: " + "link_to_admin_panel")
-            mailer.send(message)
-        elif request.POST.getone('task')=="edit":
-            # update info in database
-            print "updated"
-    return Registration_View.pushTemplate('Registration.mako')
+'''
 
 def MainPage(request):
     return Registration_View.pushTemplate('MainPage.txt')
@@ -71,6 +35,7 @@ def Workspace(request):
 
 def Tasks(request):
     return Registration_View.pushTemplate('Admin.txt')
+'''
 
 def main(global_config, **settings):
     config = Configurator(settings=settings)
@@ -83,31 +48,31 @@ def main(global_config, **settings):
     #    config.add_route(child.find('name').text, child.find('path').text)
     #    config.add_view(child.find('module'), route_name=child.find('name').text)
     config.add_route('Login', '/')
-    config.add_view(Login , route_name='Login')
+    #config.add_view(Login , route_name='Login')
 
     config.add_route('Registration', '/registration')
-    config.add_view(Registration, route_name='Registration')
+    #config.add_view(Registration, route_name='Registration')
 
     config.add_route('MainPage', '/main')
-    config.add_view(MainPage, route_name='MainPage')
+    #config.add_view(MainPage, route_name='MainPage')
 
     config.add_route('Blog', '/blog')
-    config.add_view(Blog, route_name='Blog')
+    #config.add_view(Blog, route_name='Blog')
 
     config.add_route('Files', '/files')
-    config.add_view(Files, route_name='Files')
+    #config.add_view(Files, route_name='Files')
 
     config.add_route('Calendar', '/calendar')
-    config.add_view(Calendar, route_name='Calendar')
+    #config.add_view(Calendar, route_name='Calendar')
 
     config.add_route('Workspace', '/workspace')
-    config.add_view(Workspace, route_name='Workspace')
+    #config.add_view(Workspace, route_name='Workspace')
 
     config.add_route('Admin', '/DoNotPutAdmin')
-    config.add_view(Workspace, route_name='Admin')
+    #config.add_view(Workspace, route_name='Admin')
 
     config.add_route('Tasks', '/tasks')
-    config.add_view(Workspace, route_name='Tasks')
+    #config.add_view(Workspace, route_name='Tasks')
 
     #needed for pyramid mailer
     config.include('pyramid_mailer')
