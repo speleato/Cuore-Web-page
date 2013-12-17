9e540e64535c6b76ad8631221e45ec0614aaf9ec
klinvill <kjlinvill@gmail.com>
2013-12-16 16:58:23 -0800

    Implemented Registration Form (not linked to database yet and email doesn't actually send yet)

--- pycharm_pyramid/Views/Registration_View.py
+++ pycharm_pyramid/Views/Registration_View.py
@@ -3,8 +3,6 @@ from mako.runtime import Context
 from StringIO import StringIO
 from mako.lookup import TemplateLookup
 from pyramid.response import Response
-from pyramid_mailer import get_mailer
-from pyramid_mailer.message import Message
 
 def pushTemplate(templatename):
     mylookup = TemplateLookup(directories=['Views/templates'])
@@ -12,11 +10,3 @@ def pushTemplate(templatename):
     return Response(mytemplate.render())
 
 
-#after registration, send email to leo
-#mailer=get_mailer(request)
-#message = Message(subject="Registration by "+name,
-#                  sender=cuore_email,
-#                  recipients=["kirby@cuore.io"], #change to leo when rolled out
-#                  body=name+" has registered for the Cuore Intranet with the email "+email+". Click here to confirm "
-#                       +name+"'s registration: "+link_to_admin_panel)
-#mailer.send(message)
--- pycharm_pyramid/main.py
+++ pycharm_pyramid/main.py
@@ -2,6 +2,8 @@ from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from Model import Model
 from Views import Login_View, Registration_View
+from pyramid_mailer import get_mailer
+from pyramid_mailer.message import Message
 
 def Login(request):
     parameters = Model.get_initial_data()
@@ -10,6 +12,43 @@ def Login(request):
 
 def Registration(request):
     parameters = Model.process_business_logic()
+    print "bingo"
+    if request.POST:
+        print "submitted"
+        if request.POST.getone('task') == "admin":
+            title=request.POST.getone('title')
+            email=request.POST.getone('email')
+            department=request.POST.getone('department')
+
+            print "admin path"
+            # add updated info to database
+            # after confirmed by Leo, send email to user
+            mailer = get_mailer(request)
+            message = Message(subject="Confirm your Cuore Intranet Registration",
+                  sender = "kirby@cuore.io",                    # change to admin email later
+                  recipients = [email],
+                  body = "You have been added to the Cuore intranet as a " + title + " in the "
+                       + department + " department. Click on this link to confirm your"
+                       + "registration: " + "user_link")
+            mailer.send(message)
+        elif request.POST.getone('task') == "create":
+            name=request.POST.getone('firstName') + " " + request.POST.getone('lastName')
+            email=request.POST.getone('email')
+
+            print "created"
+            # create user node in database, put in temporary zone
+            # after registration, send email to Leo
+            mailer=get_mailer(request)
+            message = Message(subject="Registration by " + name,
+                              sender="kirby@cuore.io",      # change to cuore mail server later
+                              recipients=["kirby@cuore.io"],  # change to leo when rolled out
+                              body=name + " has registered for the Cuore Intranet with the email "
+                                     + email + ". Click here to confirm " + name
+                                     + "'s registration: " + "link_to_admin_panel")
+            mailer.send(message)
+        elif request.POST.getone('task')=="edit":
+            # update info in database
+            print "updated"
     return Registration_View.pushTemplate('Registration.mako')
 
 def MainPage(request):
@@ -69,6 +108,9 @@ if __name__ == '__main__':
 
     config.add_route('Tasks', '/tasks')
     config.add_view(Workspace, route_name='Tasks')
+
+    #needed for pyramid mailer
+    config.include('pyramid_mailer')
 
     app = config.make_wsgi_app()
     server = make_server('0.0.0.0', 8080, app)
