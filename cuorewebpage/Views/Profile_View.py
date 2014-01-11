52a329b06ad9d25cbfa5dddfd81e49d3bb667d9e
Sergio <speleato@gmail.com>
2014-01-11 15:42:08 -0800

    Session upload

--- cuorewebpage/Views/Profile_View.py
+++ cuorewebpage/Views/Profile_View.py
@@ -2,10 +2,14 @@ from pyramid.view import view_config
 
 from py2neo import neo4j, ogm
 from database_config import db_config
+from cuorewebpage.lib.session import *
 
 graph_db = neo4j.GraphDatabaseService(db_config['uri'])
 store = ogm.Store(graph_db)
 
 @view_config(route_name="Profile", renderer="cuorewebpage:templates/profile.mako")
 def profile(request):
-    return {}
+    if isUserLoggedOn(request):
+        return {}
+    else:
+        return redirectUser(request)
