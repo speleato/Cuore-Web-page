f5b5e4eac92cd2cc14e70f040dfb10658d299ade
Sergio <speleato@gmail.com>
2014-01-11 15:44:09 -0800

    Session upload

--- cuorewebpage/Views/dashboard_view.py
+++ cuorewebpage/Views/dashboard_view.py
@@ -1,10 +1,14 @@
 from pyramid.view import view_config
 from pyramid.response import Response
+from cuorewebpage.lib.session import *
 
 @view_config(route_name='Dashboard', renderer='cuorewebpage:templates/dashboard.mako')
 def dashboard(request):
-    ctx = {}
-    ctx['section'] = 'Dashboard'
-    return ctx
+    if isUserLoggedOn(request):
+        ctx = {}
+        ctx['section'] = 'Dashboard'
+        return ctx
+    else:
+        return redirectUser(request)
 
 
