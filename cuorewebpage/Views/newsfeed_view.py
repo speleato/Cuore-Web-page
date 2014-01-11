f13a8c4f8e275a0c55326df4312cbf7f7dd04c75
Sergio <speleato@gmail.com>
2014-01-11 15:42:32 -0800

    Session upload

--- cuorewebpage/Views/newsfeed_view.py
+++ cuorewebpage/Views/newsfeed_view.py
@@ -4,18 +4,22 @@ import time
 
 from py2neo import neo4j, ogm
 from database_config import db_config
+from cuorewebpage.lib.session import *
 
 graph_db = neo4j.GraphDatabaseService(db_config['uri'])
 store = ogm.Store(graph_db)
 
 @view_config(route_name="Newsfeed", renderer="cuorewebpage:templates/newsfeed.mako")
 def Newsfeed(request):
-    if request.POST:
-        newsNode=graph_db.create({"news":request.POST.getone('news'), "time":time.time(), "author":request.POST.getone('author')})
-        departments=request.POST.getall('postTo[]')
-        for i in departments:
-            depNode=graph_db.get_indexed_node("Newsfeed", "name", i)
-            number=depNode.get_properties()["numPosts"]
-            depNode.update_properties({"numPosts":(number+1)})
-            graph_db.create((depNode, "NEWS", newsNode[0]))
-    return {}
+    if isUserLoggedOn(request):
+        if request.POST:
+            newsNode=graph_db.create({"news":request.POST.getone('news'), "time":time.time(), "author":request.POST.getone('author')})
+            departments=request.POST.getall('postTo[]')
+            for i in departments:
+                depNode=graph_db.get_indexed_node("Newsfeed", "name", i)
+                number=depNode.get_properties()["numPosts"]
+                depNode.update_properties({"numPosts":(number+1)})
+                graph_db.create((depNode, "NEWS", newsNode[0]))
+        return {}
+    else:
+        return redirectUser(request)
