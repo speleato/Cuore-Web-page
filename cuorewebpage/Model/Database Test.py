b99b799bce44a15bb9cab3149580e44eda8dc686
Vincente Ciancio <vciancio@socaldevs.com>
2014-01-11 16:49:01 -0800

    commiting before the .idea haunts me from the grave...

---
+++ cuorewebpage/Model/Database Test.py
@@ -0,0 +1,7 @@
+from cuorewebpage.Model.Blog import Blog
+from cuorewebpage.Model.Post import Post
+
+__author__ = 'vincente'
+
+mBlog   = Blog(Name="Applications Blog")
+mPost1  = Post(Name="Getting Stuff Done!")
